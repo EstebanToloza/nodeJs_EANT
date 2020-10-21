146e191b794971db6980d9f7b99903f2c50c9245
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-10-20 22:16:27 -0300

    Sirviendo archivos estáticos

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const http = require('http')
+const fs = require('fs')
 
 const port = 1000
 
 const server = (req, res) => {
-    res.end("I am you Script!")
+
+    fs.readFile('front/index.html', (error, content) => {     //
+        if(error){
+            res.writeHead(404, { "Content-Type" : "text/plain" })
+            res.end(`Malió sal...`)
+
+        } else {
+            res.writeHead(404, { "Content-Type" : "text/html" })
+            res.end(content)
+        }
+    }) 
+
+    //res.end("I am you Script!")
 }
 
 http.createServer( server ).listen(port)
