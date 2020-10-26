7fa24937f577193328674a6de570c148994ec10e
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-10-26 16:14:40 -0300

    Instalando express

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const http = require('http')
+const http = require('http') //se incluyen los módulos para trabajar desde el navegador. El módulo http captura toda petición realizada al servidor desde el puerto configurado
 const fs = require('fs')
 
-const port = 1000
+const port = 1000 //
 
-const server = (req, res) => {
+const server = (req, res) => { //parámetros para configurar el servidor. La función server es la que 
 
     fs.readFile('front/index.html', (error, content) => {     //
         if(error){
-            res.writeHead(404, { "Content-Type" : "text/plain" })
+            res.writeHead(404, { "Content-Type" : "text/plain" }) //Se configura la respuesta en caso de error
             res.end(`Malió sal...`)
 
         } else {
-            res.writeHead(404, { "Content-Type" : "text/html" })
+            res.writeHead(200, { "Content-Type" : "text/html" }) //Se configura la respuesta positiva
             res.end(content)
         }
     }) 
