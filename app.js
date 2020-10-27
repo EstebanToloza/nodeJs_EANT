a9ed3e0d44d0c53618c203d5dd0400ba1d3c1f40
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-10-27 17:07:10 -0300

    Prototipo del backend

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 1000 //
 
 app.listen(port)
 app.use( express.static('public') )
+app.use( express.urlencoded({ extended : true })) //Convirtiendo el contenido a un objeto
 
 /* PLANTILLA MODELO PARA "ENDPOINTS" DE EXPRESS()
 app.TIPO_HTTP('/peliculas.html', (req, res) => {
@@ -16,5 +17,7 @@ app.get('/contacto', (req, res) => {
 })
 
 app.post('/enviar', (req, res) => {
+    const contacto = req.body
+    console.log(contacto)
     res.end('Desde acá salee...')
 })
