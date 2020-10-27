41f690035d37124fe8b2b84e0e20a4c03c0d353a
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-10-27 16:42:40 -0300

    Creando Front de formulario de contacto

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const app = express()
 
 const port = 1000 //
 
-
-
 app.listen(port)
+app.use( express.static('public') )
 
 /* PLANTILLA MODELO PARA "ENDPOINTS" DE EXPRESS()
 app.TIPO_HTTP('/peliculas.html', (req, res) => {
@@ -14,4 +13,8 @@ app.TIPO_HTTP('/peliculas.html', (req, res) => {
 
 app.get('/contacto', (req, res) => {
     res.end('Desde acá vamos a contactarnos...')
+})
+
+app.post('/enviar', (req, res) => {
+    res.end('Desde acá salee...')
 })
