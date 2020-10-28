f9c287c8c7ee5edd29c7fd8efea77e0688223c92
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-10-27 21:26:00 -0300

    Incorporando dotenv para variables de entorno

--- app.js
+++ app.js
@@ -1,45 +1,4 @@
-/*const express = require('express')
-const nodemailer = require('nodemailer')
-const app = express()
-
-const port = 1000 //
-
-const miniOutlook = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'elisha.cummings65@ethereal.email',
-        pass: 'UWUp2FAgZ8xCvr7QET'
-    },
-});
-
-app.listen(port)
-app.use( express.static('public') )
-app.use( express.urlencoded({ extended : true })) //Convirtiendo el contenido a un objeto */
-
-/* PLANTILLA MODELO PARA "ENDPOINTS" DE EXPRESS()
-app.TIPO_HTTP('/peliculas.html', (req, res) => {
-
-}) */
-/*
-app.post('/enviar', (req, res) => {
-    const contacto = req.body
-
-    miniOutlook.sendMail({
-        from: contacto.correo, // sender address
-        to: "estebanisaiastoloza@gmail.com", // list of receivers
-        subject: `Asunto #${contacto.asunto}`, // Subject line
-        html: `<blockquote>${contacto.mensaje}</blockquote>`, // html body
-    });
-
-    //console.log(contacto)
-    res.end('Desde acá vamos a enviar un email de contacto :o')
-}) */
-
-
-
-
-
+require('dotenv').config() 
 const express = require("express")
 const nodemailer = require("nodemailer")
 const app = express()
@@ -47,11 +6,11 @@ const app = express()
 const port = 1000   //mas allá del 1000 usualmente están disponibles
 
 const miniOutlook = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+    host: process.env.HOST_EMAIL,
+    port: process.env.PUERTO_EMAIL,
     auth: {
-        user: 'elisha.cummings65@ethereal.email',
-        pass: 'UWUp2FAgZ8xCvr7QET'
+        user: process.env.CASILLA_EMAIL,
+        pass: process.env.CLAVE_EMAIL
     }
 })
 
@@ -70,6 +29,7 @@ app.post("/enviar", (req, res) => {
     miniOutlook.sendMail({
         from : contacto.correo, // sender address
         to : "estebanisaiastoloza@gmail.com", // list of receivers
+        replyTo : contacto.correo, 
         subject : `Asunto #${contacto.asunto}`, // Subject line
         html : `<blockquote>${contacto.mensaje}</blockquote>"`, // html body
     })
---
+++ test.js
@@ -0,0 +1,5 @@
+require('dotenv').config()
+
+console.log(process.env.PASS_SUPER_SECRETA)
+console.log(process.env.PUERTO_EMAIL)
+
